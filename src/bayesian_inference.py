484172880723ab668800bcc854935cce5fc709ca
Abenezer <abenezernigussiecs@gmail.com>
2025-02-21 22:42:53 +0300

    Improved documentation in Bayesian inference module

--- src/bayesian_inference.py
+++ src/bayesian_inference.py
@@ -3,18 +3,43 @@ import numpy as np
 
 
 class BayesianModel:
-    def __init__(self, data):
+    """
+    A class to perform Bayesian inference on time series data using PyMC3.
+
+    Attributes:
+        data (pd.DataFrame): The dataset containing time series data.
+
+    Methods:
+        fit_bayesian_model(): Fits a Bayesian model to the data and returns the trace.
+    """
+
+    def __init__(self, data: pd.DataFrame):
+        """
+        Initializes the BayesianModel class with the given dataset.
+
+        Parameters:
+            data (pd.DataFrame): Dataset containing time series data.
+        """
         self.data = data
 
     def fit_bayesian_model(self):
+        """
+        Fits a Bayesian model to the time series data using PyMC3.
+
+        The model assumes a normal likelihood with priors on the mean (`mu`) and standard deviation (`sigma`).
+
+        Returns:
+            trace: The posterior samples from the Bayesian inference.
+        """
         with pm.Model() as model:
             # Priors
-            mu = pm.Normal("mu", mu=0, sigma=1)
-            sigma = pm.HalfNormal("sigma", sigma=1)
+            mu = pm.Normal("mu", mu=0, sigma=1)  # Prior for the mean
+            sigma = pm.HalfNormal("sigma", sigma=1)  # Prior for the standard deviation
 
-            # Likelihood
+            # Likelihood (observed data)
             y = pm.Normal("y", mu=mu, sigma=sigma, observed=self.data["Price"])
 
-            # Sampling
+            # Sampling from the posterior
             trace = pm.sample(1000, return_inferencedata=True)
+
         return trace
