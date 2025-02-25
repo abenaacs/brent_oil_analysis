5a133867cf80c71f3e9a147b5caa29d3f0c1900a
Abenezer <abenezernigussiecs@gmail.com>
2025-02-25 23:49:23 +0300

    Created React component for visualizing oil prices

---
+++ dashboard/frontend/src/components/OilPriceChart.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const data = [
+  { date: "2023-01-01", price: 80.5 },
+  { date: "2023-01-02", price: 81.0 },
+];
+
+function OilPriceChart() {
+  return (
+    <LineChart width={600} height={300} data={data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="price" stroke="#8884d8" />
+    </LineChart>
+  );
+}
+
+export default OilPriceChart;
